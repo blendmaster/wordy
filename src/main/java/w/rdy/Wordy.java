99a6d2fc0c045e157addecb96043bfd37f51c63f
Steven Ruppert <blendmaster@gmail.com>
2013-07-23 22:16:26 -0600

    Reformat Wordy.java
    On average 2 spaces per line!

--- src/main/java/w/rdy/Wordy.java
+++ src/main/java/w/rdy/Wordy.java
@@ -1,13 +1,15 @@
 package w.rdy;
 
 public class Wordy {
-  public static interface Fn {
-    public int apply(int input);
 
-  }
-  public static void main(String[] args) {
-    Fn square = i -> i * i;
-    System.out.println("Wordy: " + square.apply(2));
-    System.exit(0);
-  }
+public static interface Fn {
+  public int apply(int input);
+
+}
+
+public static void main(String[] args) {
+  Fn square = i -> i * i;
+  System.out.println("Wordy: " + square.apply(2));
+  System.exit(0);
+}
 }
